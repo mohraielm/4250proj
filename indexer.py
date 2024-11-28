3b90ee09c444307c43cc97fd619bb49d5b18b2f4
yuunster <kayuuha@gmail.com>
2024-11-28 12:08:22 -0800

    Update comments and comment our console log debugging

--- indexer.py
+++ indexer.py
@@ -46,14 +46,14 @@ def index(texts: list[str]):
         index_collection.update_one(
             { "_id": doc_id },
             { "$set": { "sparseVector": sparse_vector_to_dict } },
-            upsert=True
+            upsert=True  # Create if doesn't exist
         )
 
 
     # retrieve the terms after tokenization, stopword removal, and stemming
     terms = vectorizer.get_feature_names_out()
 
-    # showing the term matrix in an organized way by using a data frame
+    # Print term matrix using a dataframe for console print formatting
     # print("TD-IDF Vectorizer Training\n")
     # print(pd.DataFrame(data = sparse_matrix.toarray(), columns = tokens))
 
@@ -74,18 +74,18 @@ def index(texts: list[str]):
                 for doc_id in doc_indices
             ]
 
-    # Convert to DataFrame so it prints nicely in console for debugging
-    inverted_index_df = pd.DataFrame([
-        {"term": term, "documents": info}
-        for term, info in inverted_index.items()
-    ])
-    print("Inverted Index\n")
-    print(inverted_index_df)
+    # Print inverted index using a dataframe for console print formatting
+    # inverted_index_df = pd.DataFrame([
+    #     {"term": term, "documents": info}
+    #     for term, info in inverted_index.items()
+    # ])
+    # print("Inverted Index\n")
+    # print(inverted_index_df)
 
     # Store each term and its info as its own document in the index collection
     for term, info in inverted_index.items():
         inverted_index_collection.update_one(
             { "_id": term },
-            {"$set": {"documents": info}},  # Update or insert
+            { "$set": { "documents": info } },
             upsert=True  # Create if doesn't exist
         )
