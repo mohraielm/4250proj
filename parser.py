dddea64b591fa87c5ac7c0f25fd5745eee878d85
erikajledesma <erika.jasmine06@gmail.com>
2024-12-01 21:09:10 -0800

    extracted text combined into one text to insert into mongodb

--- parser.py
+++ parser.py
@@ -12,17 +12,19 @@ for target in targets:
     # fac-staff and accolades are the search areas. need to extract just text
 
     fac_staff = bs.find('div', {'class': 'fac-staff'}).find_all('div', {'class': 'col'})
-    fac_staff_text = [' '.join(staff.get_text(strip=True).split()) for staff in fac_staff]
+    fac_staff_list = [' '.join(staff.get_text(strip=True).split()) for staff in fac_staff]
 
     # might have to come back to this so that only text is extracted
     accolades = bs.find('div', {'class': 'accolades'})
-    accolades_text = [' '.join(accolade.get_text(strip=True).split()) for accolade in accolades]
+    accolades_list = [' '.join(accolade.get_text(strip=True).split()) for accolade in accolades]
+    
+    fac_staff_str = ' '.join(fac_staff_list)
+    accolade_str = ' '.join(accolades_list)
 
-    combined_list = []
-    combined_list = fac_staff_text + accolades_text
-    cleaned_list = [item for item in combined_list if item not in (None, '')]
-    print(cleaned_list)
+    combined = ''
+    combined += fac_staff_str
+    combined += accolade_str
 
     # insert into mongodb
-    search_content.insert_one({'text': combined_list})
+    search_content.insert_one({'text': combined})
 
